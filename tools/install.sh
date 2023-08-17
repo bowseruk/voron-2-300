f15f2d35f9c72dc60119d9dcafd01859b13dfdb3
BowserUK <13262235+bowseruk@users.noreply.github.com>
2023-08-17 14:59:00 +0100

    Bug fixes

--- tools/install.sh
+++ tools/install.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # This is the directory for this repo and then other used repos
-repo="~/voron-2-300"
-klipper="~/klipper"
-katapult="~/katapult"
+repo="$HOME/voron-2-300"
+klipper="$HOME/klipper"
+katapult="$HOME/katapult"
 network="/etc/network/interfaces.d"
-printerdata="~/printer_data"
+printerdata="$HOME/printer_data"
 
 # Setup the canbus network ready for the Octopus canbus bridge
-cp "${repo}/network/*" ${network}
+sudo cp ${repo}/network/* ${network}
 
 # Get a placeholder to make a unique folder
 backupfolder=$(date +%Y%m%d_%H%M%S)
 
 # Dump current contents of config and copy the linked printer.cfg
-mkdir "${repo}/config/backup/${backupfolder}"
-cp "${printerdata}/config/*" "${repo}/config/backup/${backupfolder}"
+mkdir -p ${repo}/config/printer_data/backup/${backupfolder}
+cp ${printerdata}/config/* ${repo}/config/printer_data/backup/${backupfolder}
 
 # Setup the printer config files
-cp "${repo}/printer_data/install/printer.cfg" "${printerdata}/config"
+cp ${repo}/config/printer_data/install/printer.cfg ${printerdata}/config
