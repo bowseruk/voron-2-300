ae48b71f45b5d8fffceeac3a4809f73662b58bd2
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-08-17 13:27:03 +0100

    Updated config files to be modular

--- tools/install.sh
+++ tools/install.sh
@@ -8,7 +8,14 @@ network="/etc/network/interfaces.d"
 printerdata="~/printer_data"
 
 # Setup the canbus network ready for the Octopus canbus bridge
-cp "${repo}/network/can0" ${network}
+cp "${repo}/network/*" ${network}
+
+# Get a placeholder to make a unique folder
+backupfolder=$(date +%Y%m%d_%H%M%S)
+
+# Dump current contents of config and copy the linked printer.cfg
+mkdir "${repo}/config/backup/${backupfolder}"
+cp "${printerdata}/config/*" "${repo}/config/backup/${backupfolder}"
 
 # Setup the printer config files
-cp "${repo}/printer_data/*" ${printerdata}
+cp "${repo}/printer_data/install/printer.cfg" "${printerdata}/config"
